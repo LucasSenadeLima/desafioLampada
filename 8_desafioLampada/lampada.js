75d34c0baa6098ab42eaf2c126e8a96bf393af41
LucasSenadeLima <lucassenabio@gmail.com>
2023-03-24 17:15:43 -0300

    projeto lampada finalizado

--- 8_desafioLampada/lampada.js
+++ 8_desafioLampada/lampada.js
@@ -1,16 +1,28 @@
 var lamp = window.document.getElementById('lampada')
 
+function estaQuebrada(){
+    return lamp.src.indexOf('quebrada') > -1
+}
+
 
 function Ligar(){
+    if (!estaQuebrada()){
     lamp.src ='ligada.svg'
-    window.document.body.style.backgroundColor = '#fecd00'
+    window.document.body.style.backgroundColor = '#fecd00'}
 }
 function Desligar(){
+    if (!estaQuebrada()){
     lamp.src ='desligada.svg'
-    window.document.body.style.backgroundColor = 'rgb(48,48,48)'
+    window.document.body.style.backgroundColor = 'rgb(48,48,48)'}
 }
 
+lamp.addEventListener ('click', Quebrar)
 function Quebrar(){
     lamp.src = 'quebrada.svg'
-    window.document.body.style.backgroundColor = 'white'
+    window.document.body.style.backgroundColor = 'black'
+}
+
+function Consertar(){
+    if (estaQuebrada()){
+    lamp.src = 'desligada.svg'}
 }
